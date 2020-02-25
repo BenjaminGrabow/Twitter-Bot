25589ec6f9002a0c679e83e71cb080ad36f6ecb9
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-25 21:58:31 +0100

    add login method

--- app.py
+++ app.py
@@ -12,3 +12,13 @@ class TwitterBot:
     bot = self.bot
     bot.get('https://twitter.com')
     time.sleep(3)
+
+    email = bot.find_element_by_name('session[username_or_email]')
+    password = bot.find_element_by_name('session[password]')
+    email.clear()
+    password.clear()
+    email.send_keys(self.username)
+    password.send_keys(self.password)
+    password.send_keys(Keys.RETURN)
+    time.sleep(3)
+
