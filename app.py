b3a3aff7bbf8cbab6076660adaa3fe9b02c5835b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-25 21:34:32 +0100

    create Twitter Bot class

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+class TwitterBot:
+  def __init__(self, username, password):
+    self.username = username
+    self.password = password
+    self.bot = webdriver.Firefox()
+
