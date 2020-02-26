3b0b676ba44957e5417c0548abf11f57f11070a8
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-26 20:59:03 +0100

    add like function

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ class TwitterBot:
         bot.get('https://twitter.com/search?q=%23' +
                 hashtag + '&src=typed_query')
         time.sleep(3)
-        for i in range(1, 1000):
+        for i in range(1, 3):
             bot.execute_script(
                 'window.scrollTo(0, document.body.scrollHeight)')
             time.sleep(2)
@@ -38,13 +38,74 @@ class TwitterBot:
                 bot.get(link)
                 time.sleep(5)
                 try:
-                    bot.find_element_by_xpath("//div[contains(@data-testid, 'follow')]").click()
+                    bot.find_element_by_xpath("//div[not(contains(@data-testid,'unfollow')) and contains(@data-testid, 'follow')]").click()
                     time.sleep(15)
                 except Exception as ex:
                     print('FAILED')
-                    time.sleep(30)
+                    time.sleep(2)
+    
+    def like(self, hashtag):
+        bot = self.bot
+        bot.get('https://twitter.com/search?q=%23' +
+                hashtag + '&src=typed_query')
+        time.sleep(3)
+        for i in range(1, 3):
+            bot.execute_script(
+                'window.scrollTo(0, document.body.scrollHeight)')
+            time.sleep(2)
+            tweetLinks = [i.get_attribute('href') for i in bot.find_elements_by_xpath("//a[@dir='auto']")]
+            filteredLinks = list(filter(lambda x: 'status' in x, tweetLinks))
+            for link in filteredLinks:
+                bot.get(link)
+                time.sleep(5)
+                try:
+                    bot.find_element_by_xpath("//div[not(contains(@data-testid,'unfollow')) and contains(@data-testid, 'follow')]").click()
+                    time.sleep(15)
+                except Exception as ex:
+                    print('FAILED')
+                    time.sleep(2)
 
-ben = TwitterBot('YourUsername', 'YourPassword') # change it to your username and password
+ben = TwitterBot('username', 'password')
 
 ben.login()
-ben.follow('webdevelopment') # change this to whatever hashtag you want to 
+
+ben.follow('code')
+
+time.sleep(300)
+ben.follow('coding')
+
+
+time.sleep(600)
+ben.follow('react')
+
+
+time.sleep(900)
+ben.follow('reactnative')
+
+
+time.sleep(1200)
+ben.follow('github')
+
+
+time.sleep(1500)
+ben.follow('opensource')
+
+time.sleep(1800)
+ben.follow('computerscience')
+
+
+time.sleep(2100)
+ben.follow('webdevelopment')
+
+
+time.sleep(2400)
+ben.follow('lambdaschool')
+
+time.sleep(2700)
+ben.follow('algorithm')
+
+time.sleep(3000)
+ben.follow('algorithm')
+
+time.sleep(3300)
+ben.follow('software')
